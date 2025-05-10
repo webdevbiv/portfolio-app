04bb5406bdbf348ef1b0c4b2eb4bc1e7327222e4
Igor <webdevbiv@gmail.com>
2025-05-10 16:33:11 +0300

    FIX | ts

--- components/Main/StarBackground.tsx
+++ components/Main/StarBackground.tsx
@@ -3,14 +3,13 @@
 import React, { useState, useRef, Suspense } from "react";
 import { Canvas, useFrame } from "@react-three/fiber";
 import { Points, PointMaterial } from "@react-three/drei";
-// @ts-expect-error lib
 import * as random from "maath/random/dist/maath-random.esm";
 
 const StarBackground = (props: any) => {
-  // @ts-expect-error any
+  // @ts-expect-error library
   const ref: any = useRef();
   const [sphere] = useState(() =>
-    random.inSphere(new Float32Array(1000), { radius: 1.5 })
+    random.inSphere(new Float32Array(5000), { radius: 1.2 })
   );
 
   useFrame((state, delta) => {
@@ -34,7 +33,7 @@ const StarBackground = (props: any) => {
 };
 
 const StarsCanvas = () => (
-  <div className="w-full h-auto fixed inset-0 z-[-50]">
+  <div className="w-full h-auto fixed inset-0 z-[20]">
     <Canvas camera={{ position: [0, 0, 1] }}>
       <Suspense fallback={null}>
         <StarBackground />
--- eslint.config.mjs
+++ eslint.config.mjs
@@ -14,6 +14,7 @@ const eslintConfig = [
   {
     rules: {
       "@typescript-eslint/ban-ts-comment": "error",
+      "@typescript-eslint/no-explicit-any": "off",
     },
   },
 ];
